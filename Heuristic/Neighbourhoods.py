07fdd874ad0eeb43da8fe0104acd96fb5c7a7d40
Filipp <peter.filipp@iis.fraunhofer.de>
2025-03-06 20:06:42 +0100

    init

--- Heuristic/Neighbourhoods.py
+++ Heuristic/Neighbourhoods.py
@@ -3,15 +3,36 @@ from copy import deepcopy
 from itertools import combinations
 
 
-def calculate_start_and_endtimes_per_doc(schedule, key):
+def calculate_start_and_endtimes_per_doc(schedule, key, start_time=0):
     for i in range(len(schedule[key])):
-        if i == 0:
+        if i == 0 and start_time == 0:
             schedule[key][0, [4]] = schedule[key][0, [1]] + schedule[key][0, [2]]
             schedule[key][0, [3]] = schedule[key][0, [1]]
+        elif i == 0 and start_time > 0:
+            schedule[key][0, [4]] = start_time + schedule[key][0, [8]]
+            schedule[key][0, [3]] = start_time
         else:
             schedule[key][i, [3]] = max(schedule[key][i-1, [4]], schedule[key][i, [1]])
             schedule[key][i, [4]] = schedule[key][i, [3]] + schedule[key][i, [2]]
     
+    # Berechne das Maximum aus 0 und der Differenz der 4. und 6. Spalte
+    result = np.maximum(0, schedule[key][:, 3] - schedule[key][:, 5])
+
+    # Weisen Sie das Ergebnis der 7. Spalte zu
+    schedule[key][:, 6] = result
+
+def calculate_start_and_endtimes_per_doc_non_det(schedule, key, start_time=0):
+    for i in range(len(schedule[key])):
+        if i == 0 and start_time == 0:
+            schedule[key][0, [4]] = schedule[key][0, [1]] + schedule[key][0, [2]]
+            schedule[key][0, [3]] = schedule[key][0, [1]]
+        elif i == 0 and start_time > 0:
+            schedule[key][0, [4]] = start_time + schedule[key][0, [8]]
+            schedule[key][0, [3]] = start_time
+        else:
+            schedule[key][i, [3]] = max(schedule[key][i-1, [4]], schedule[key][i, [1]])
+            schedule[key][i, [4]] = schedule[key][i, [3]] + schedule[key][i, [8]]
+
     # Berechne das Maximum aus 0 und der Differenz der 4. und 6. Spalte
     result = np.maximum(0, schedule[key][:, 3] - schedule[key][:, 5])
 
@@ -26,10 +47,13 @@ def weighted_tardiness_per_doc(schedule, key):
     # Multipliziere die beiden Spalten elementweise und summiere die Ergebnisse
     return np.sum(Tardiness * weight)
 
-def swap_patients_by_doc_N1(schedule, key, patient_1, patient_2, Max_d):
+def swap_patients_by_doc_N1(schedule, key, patient_1, patient_2, Max_d,deterministic, start_time=0):
     temp_schedule = deepcopy(schedule)
     temp_schedule[key][[patient_1, patient_2]] = temp_schedule[key][[patient_2, patient_1]]
-    calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key)
+    if deterministic:
+        calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key, start_time=start_time)
+    else:
+        calculate_start_and_endtimes_per_doc_non_det(schedule=temp_schedule, key=key, start_time=start_time)
 
     # Prüfen, ob beide Bedingungen erfüllt sind
     if feasibility_check(temp_schedule, key, patient_1, Max_d=Max_d) and feasibility_check(temp_schedule, key, patient_2, Max_d=Max_d):
@@ -37,7 +61,7 @@ def swap_patients_by_doc_N1(schedule, key, patient_1, patient_2, Max_d):
     else:
         return schedule  # Ursprüngliches Schedule zurückgeben, wenn eine Bedingung nicht erfüllt ist
 
-def insert_patient_by_doc_N2(schedule, key, insert_patient_id, in_front_of_patient_id, Max_d):
+def insert_patient_by_doc_N2(schedule, key, insert_patient_id, in_front_of_patient_id, Max_d,deterministic, start_time=0):
     temp_schedule = deepcopy(schedule)
     
     # Bestimme die Indizes des zu verschiebenden und des Zielpatienten basierend auf dem Wert in der ersten Spalte
@@ -57,7 +81,10 @@ def insert_patient_by_doc_N2(schedule, key, insert_patient_id, in_front_of_patie
     # Füge den Patienten vor dem Zielpatienten ein
     temp_schedule[key] = np.insert(temp_schedule[key], in_front_of_index, patient_data, axis=0)
     
-    calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key)
+    if deterministic:
+        calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key, start_time=start_time)
+    else:
+        calculate_start_and_endtimes_per_doc_non_det(schedule=temp_schedule, key=key, start_time=start_time)
 
     # Neue Indizes nach Insert für patient_1 und Patient_2
     patient_1 = np.where(temp_schedule[key][:, 0] == insert_patient_id)[0][0]
@@ -70,7 +97,7 @@ def insert_patient_by_doc_N2(schedule, key, insert_patient_id, in_front_of_patie
         return schedule  # Ursprüngliches Schedule zurückgeben, wenn eine Bedingung nicht erfüllt ist
 
 
-def swap_patients_between_docs_N3(schedule, patient_1, patient_2, Max_d):
+def swap_patients_between_docs_N3(schedule, patient_1, patient_2, Max_d, deterministic, start_time_1=0, start_time_2=0):
     # patient_1 und patient_2 sind in der Form [key, patient_index]
     temp_schedule = deepcopy(schedule)
 
@@ -86,8 +113,12 @@ def swap_patients_between_docs_N3(schedule, patient_1, patient_2, Max_d):
     temp_schedule[key2][index2, :] = patient_data_1
 
     # Neuberechnung der Start- und Endzeiten für beide betroffenen Doktoren
-    calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key1)
-    calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key2)
+    if deterministic:
+        calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key1, start_time=start_time_1)
+        calculate_start_and_endtimes_per_doc(schedule=temp_schedule, key=key2, start_time=start_time_2)
+    else:
+        calculate_start_and_endtimes_per_doc_non_det(schedule=temp_schedule, key=key1, start_time=start_time_1)
+        calculate_start_and_endtimes_per_doc_non_det(schedule=temp_schedule, key=key2, start_time=start_time_2)
 
         # Prüfen, ob beide Bedingungen erfüllt sind
     if feasibility_check(temp_schedule, key1, index1, Max_d=Max_d) and feasibility_check(temp_schedule, key2, index2, Max_d=Max_d):
@@ -96,28 +127,34 @@ def swap_patients_between_docs_N3(schedule, patient_1, patient_2, Max_d):
         return schedule  # Ursprüngliches Schedule zurückgeben, wenn eine Bedingung nicht erfüllt ist
 
 
-def local_search(schedule, neighborhood, Max_d=120):
+def local_search(schedule, start_time_array ,neighborhood, Max_d=120, deterministic=True):
     temp_schedule = deepcopy(schedule)
     overall_tardiness = 0
-
     if neighborhood == "N1":
         for key in schedule:
-            temp_schedule, lowest_tardiness = best_swap_N1(temp_schedule, key, Max_d=Max_d)
-            overall_tardiness += lowest_tardiness
+            if schedule[key].shape[0] > 1:
+                start_time = start_time_array[key-1]
+                temp_schedule, lowest_tardiness = best_swap_N1(temp_schedule, key, Max_d=Max_d, start_time=start_time, deterministic=deterministic)
+                #overall_tardiness += lowest_tardiness
     if neighborhood == "N2":
         for key in schedule:
-            temp_schedule, lowest_tardiness = best_insert_N2(temp_schedule, key, Max_d=Max_d)
-            overall_tardiness += lowest_tardiness
+            if schedule[key].shape[0] > 1:
+                start_time = start_time_array[key-1]
+                temp_schedule, lowest_tardiness = best_insert_N2(temp_schedule, key, Max_d=Max_d, start_time=start_time, deterministic=deterministic)
+                #overall_tardiness += lowest_tardiness
     if neighborhood == "N3":
         # Erhalte alle möglichen Paare von Keys
         keys = list(temp_schedule.keys())
         key_pairs = combinations(keys, 2)
         # Durchlaufe alle möglichen Paare von Ärzten und führe den iterativen Swap durch
         for key1, key2 in key_pairs:
-            temp_schedule, lowest_tardiness = best_swap_N3(temp_schedule, key1, key2)
+            if schedule[key1].shape[0] > 0 and schedule[key2].shape[0] > 0:
+                start_time_1 = start_time_array[key1-1]
+                start_time_2 = start_time_array[key2-1]
+                temp_schedule, lowest_tardiness = best_swap_N3(temp_schedule, key1, key2, start_time_1=start_time_1, start_time_2=start_time_2, deterministic=deterministic)
         
-        for key in temp_schedule:
-            overall_tardiness += weighted_tardiness_per_doc(temp_schedule, key)
+    for key in temp_schedule:
+        overall_tardiness += weighted_tardiness_per_doc(temp_schedule, key)
 
     return temp_schedule, overall_tardiness
 
@@ -128,7 +165,7 @@ def feasibility_check(schedule, key, index, Max_d):
 
     
 
-def best_swap_N1(schedule, key, Max_d = 120):
+def best_swap_N1(schedule, key, deterministic, Max_d = 120, start_time=0):
     # Anzahl der Patienten bei diesem Doktor
     num_patients = schedule[key].shape[0]
     
@@ -141,7 +178,7 @@ def best_swap_N1(schedule, key, Max_d = 120):
         for j in range(i + 1, num_patients):
             # Führe den Tausch durch
             temp_schedule = deepcopy(schedule)
-            temp_schedule = swap_patients_by_doc_N1(temp_schedule, key, i, j, Max_d=Max_d)
+            temp_schedule = swap_patients_by_doc_N1(temp_schedule, key, i, j, Max_d=Max_d, start_time=start_time, deterministic=deterministic)
             
             # Berechne den weighted tardiness für den getauschten Schedule
             tardiness = weighted_tardiness_per_doc(temp_schedule, key)
@@ -154,7 +191,7 @@ def best_swap_N1(schedule, key, Max_d = 120):
     
     return best_schedule, lowest_tardiness
 
-def best_insert_N2(schedule, key, Max_d = 120):
+def best_insert_N2(schedule, key, deterministic, Max_d = 120, start_time=0):
     # Anzahl der Patienten bei diesem Doktor
     num_patients = schedule[key].shape[0]
     
@@ -171,7 +208,7 @@ def best_insert_N2(schedule, key, Max_d = 120):
                 patient_id_target = schedule[key][j, 0]
                 
                 # Führe die Einfügeoperation durch
-                temp_schedule = insert_patient_by_doc_N2(schedule, key, patient_id_to_move, patient_id_target, Max_d=Max_d)
+                temp_schedule = insert_patient_by_doc_N2(schedule, key, patient_id_to_move, patient_id_target, Max_d=Max_d, start_time=start_time, deterministic=deterministic)
                 
                 # Berechne den weighted tardiness für den veränderten Schedule
                 tardiness = weighted_tardiness_per_doc(temp_schedule, key)
@@ -185,7 +222,7 @@ def best_insert_N2(schedule, key, Max_d = 120):
     return best_schedule, lowest_tardiness
 
 
-def best_swap_N3(schedule, key1, key2, Max_d = 120):
+def best_swap_N3(schedule, key1, key2, deterministic, Max_d = 120, start_time_1=0, start_time_2=0):
     # Anzahl der Patienten für die beiden Doktoren
     num_patients_key1 = schedule[key1].shape[0]
     num_patients_key2 = schedule[key2].shape[0]
@@ -201,7 +238,7 @@ def best_swap_N3(schedule, key1, key2, Max_d = 120):
     for i in range(num_patients_key1):
         for j in range(num_patients_key2):
             # Führe den Tausch durch
-            swapped_schedule = swap_patients_between_docs_N3(schedule, [key1, i], [key2, j], Max_d=Max_d)
+            swapped_schedule = swap_patients_between_docs_N3(schedule, [key1, i], [key2, j], Max_d=Max_d, start_time_1=start_time_1, start_time_2=start_time_2, deterministic=deterministic)
             
             # Berechne die kombinierten weighted tardiness für beide Doktoren
             combined_tardiness = (
@@ -220,7 +257,7 @@ def best_swap_N3(schedule, key1, key2, Max_d = 120):
 from copy import deepcopy
 import random
 
-def random_shake(schedule, Max_d):
+def random_shake(schedule, Max_d, start_time_array, deterministic):
     temp_schedule = deepcopy(schedule)
     
     # Wähle eine zufällige Nachbarschaftsoperation: N1, N2 oder N3
@@ -229,25 +266,33 @@ def random_shake(schedule, Max_d):
     if operation == 'N3' and len(keys)>1:
         # Randomly select two doctors and one patient from each for N3
         key1, key2 = random.sample(keys, 2)
-        index1 = random.randint(0, temp_schedule[key1].shape[0] - 1)
-        index2 = random.randint(0, temp_schedule[key2].shape[0] - 1)
-        temp_schedule = swap_patients_between_docs_N3(temp_schedule, [key1, index1], [key2, index2],  Max_d=Max_d)
+        if temp_schedule[key1].shape[0] > 0 and temp_schedule[key2].shape[0] > 0:
+            #print(f"Shake N3")
+            index1 = random.randint(0, temp_schedule[key1].shape[0] - 1)
+            index2 = random.randint(0, temp_schedule[key2].shape[0] - 1)
+            start_time_1 = start_time_array[key1-1]
+            start_time_2 = start_time_array[key2-1]
+            temp_schedule = swap_patients_between_docs_N3(temp_schedule, [key1, index1], [key2, index2],  Max_d=Max_d, start_time_1=start_time_1, start_time_2=start_time_2, deterministic=deterministic)
 
     elif operation == 'N1':
         # Randomly select a doctor and two patient indices for N1
         key = random.choice(keys)
         if temp_schedule[key].shape[0] > 1:
+            #print(f"Shake N1")
+            start_time = start_time_array[key-1]
             i, j = random.sample(range(temp_schedule[key].shape[0]), 2)
-            temp_schedule = swap_patients_by_doc_N1(temp_schedule, key, i, j, Max_d=Max_d)
+            temp_schedule = swap_patients_by_doc_N1(temp_schedule, key, i, j, Max_d=Max_d, start_time=start_time, deterministic=deterministic)
     
     elif operation == 'N2':
         # Randomly select a doctor and two patient indices for N2
         key = random.choice(keys)
         if temp_schedule[key].shape[0] > 1:
+            #print(f"Shake N2")
+            start_time = start_time_array[key-1]
             i, j = random.sample(range(temp_schedule[key].shape[0]), 2)
             patient_id_to_move = temp_schedule[key][i, 0]
             patient_id_target = temp_schedule[key][j, 0]
-            temp_schedule = insert_patient_by_doc_N2(temp_schedule, key, patient_id_to_move, patient_id_target,  Max_d=Max_d)
+            temp_schedule = insert_patient_by_doc_N2(temp_schedule, key, patient_id_to_move, patient_id_target,  Max_d=Max_d, start_time=start_time, deterministic=deterministic)
     
 
 
@@ -257,7 +302,7 @@ def random_shake(schedule, Max_d):
     
     return temp_schedule, overall_tardiness
 
-def VND(schedule, overall_tardiness, neighborhoods, Max_d=120):
+def VND(schedule, overall_tardiness, neighborhoods,start_time_array, deterministic, Max_d=120):
     current_schedule = deepcopy(schedule)
     current_tardiness = overall_tardiness
     improvement = True
@@ -267,18 +312,18 @@ def VND(schedule, overall_tardiness, neighborhoods, Max_d=120):
         for neighborhood in neighborhoods:
             # Führe die lokale Suche für die aktuelle Nachbarschaft aus
             if neighborhood == "N1":
-                new_schedule, _ = local_search(schedule, neighborhood="N1", Max_d=Max_d)
+                new_schedule, _ = local_search(schedule, neighborhood="N1", Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
             elif neighborhood == "N2":
-                new_schedule, _ = local_search(schedule, neighborhood="N2", Max_d=Max_d)
+                new_schedule, _ = local_search(schedule, neighborhood="N2", Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
             elif neighborhood == "N3":
-                new_schedule, _ = local_search(schedule, neighborhood="N3", Max_d=Max_d)
+                new_schedule, _ = local_search(schedule, neighborhood="N3", Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
             else:
                 continue
 
             # Vergleiche die neue und aktuelle Lösung
             if _ < current_tardiness:
-                current_schedule = new_schedule
-                current_tardiness = _
+                current_schedule = deepcopy(new_schedule)
+                current_tardiness = deepcopy(_)
                 improvement = True
                 break  # Beginne die Suche erneut bei der ersten Nachbarschaft
 
@@ -286,7 +331,7 @@ def VND(schedule, overall_tardiness, neighborhoods, Max_d=120):
 
 import time
 
-def general_vns(initial_schedule, initial_tardiness ,neighborhoods, Max_d=120, no_improvement_limit=1000, time_limit = 60):
+def general_vns(initial_schedule, initial_tardiness ,neighborhoods, start_time_array, Max_d=120, no_improvement_limit=1000, time_limit = 60, deterministic=True):
     current_schedule = deepcopy(initial_schedule)
     best_schedule = current_schedule
     best_schedule_tardiness = initial_tardiness
@@ -303,10 +348,9 @@ def general_vns(initial_schedule, initial_tardiness ,neighborhoods, Max_d=120, n
             return best_schedule, best_schedule_tardiness, completion_time 
         
         # Shake: Erzeugen einer neuen Lösung durch eine zufällige Veränderung
-        shaken_schedule, shaken_tardiness = random_shake(current_schedule, Max_d=Max_d)
-
+        shaken_schedule, shaken_tardiness = random_shake(current_schedule, Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
         # VND: Anwenden der lokalen Optimierung
-        vnd_schedule, vnd_tardiness = VND(shaken_schedule, shaken_tardiness ,neighborhoods, Max_d)
+        vnd_schedule, vnd_tardiness = VND(shaken_schedule, shaken_tardiness, neighborhoods=neighborhoods, Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
 
         # Bewertung der neuen Lösung
         if vnd_tardiness < best_schedule_tardiness:
@@ -323,7 +367,7 @@ def general_vns(initial_schedule, initial_tardiness ,neighborhoods, Max_d=120, n
 
 import math
 
-def simulated_annealing(schedule, total_tardiness, Imax=1000, cooling_rate=0.95, min_temperature=1.0, start_temperature=100, time_limit=600, Max_d=120):
+def simulated_annealing(schedule, total_tardiness, start_time_array, Imax=1000, cooling_rate=0.95, min_temperature=1.0, start_temperature=100, time_limit=600, Max_d=120, deterministic=True):
     current_schedule = deepcopy(schedule) # Start solution (S) is the result from the opening heuristic
     current_tardiness = deepcopy(total_tardiness) # Calculate the makespan of the start solution
     # Input seed for reproducibility if necessary
@@ -352,9 +396,9 @@ def simulated_annealing(schedule, total_tardiness, Imax=1000, cooling_rate=0.95,
             # Durchlaufe solange random_shake bis eine gültige Lösung erreicht ist
             # random_shake ist die Durchführung einer zufälligen Nachbarschaftsdefinition
             new_tardiness = current_tardiness
-            while new_tardiness == current_tardiness:
-                shaken_schedule, new_tardiness = random_shake(current_schedule, Max_d=Max_d)
-            
+            #while new_tardiness == current_tardiness:
+                #shaken_schedule, new_tardiness = random_shake(current_schedule, Max_d=Max_d, start_time_array=start_time_array)
+            shaken_schedule, new_tardiness = random_shake(current_schedule, Max_d=Max_d, start_time_array=start_time_array, deterministic=deterministic)
 
 
             # Calculate the energy difference (Difference in Tardiness)
